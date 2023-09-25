ccdf71fcc52917ba647bf07a2c9c9760f4737c58
hygrintoht <79806551+hygrintoht@users.noreply.github.com>
2023-09-25 15:54:41 +0800

    engineTime base

--- GDENG03_DX/engineTime.cpp
+++ GDENG03_DX/engineTime.cpp
@@ -1,1 +1,25 @@
 #include "engineTime.h"
+
+/*
+	unsigned long new_time = 0;
+	if (m_old_time)
+		new_time = ::GetTickCount() - m_old_time;
+	m_delta_time = new_time / 1000.0f;
+	m_old_time = ::GetTickCount();
+*/
+
+void engineTime::init()
+{
+	if (sharedInstance == nullptr)
+		sharedInstance = new engineTime();
+}
+
+double engineTime::getDeltaTime()
+{
+	return deltaTime;
+}
+
+void engineTime::logFrameStart()
+{
+	start = std::chrono::high_resolution_clock::now();
+}
